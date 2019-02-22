cd3fdfb44129ec89f15e07f68b13fc8475d16243
Trish Mederos <trishmederos@gmail.com>
2019-02-21 18:23:18 -0700

    section 11

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,20 @@
+/* Get our elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+
+/* Build our Functions */
+function togglePlay() {
+  if(video.paused){
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+/* Hook up the event listener */
