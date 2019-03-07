f16be81ab3a6aa8ec04c3fa3552fe196826b83b4
Trish Mederos <trishmederos@gmail.com>
2019-03-07 16:53:24 -0700

    section 19

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -6,7 +6,44 @@ const snap = document.querySelector('.snap');
 
 function getVideo(){
   navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-  .then(locaMediaStream => {
-    console.log(locaMediaStream);
+  .then(localMediaStream => {
+    console.log(localMediaStream);
+//  DEPRECIATION : 
+//       The following has been depreceated by major browsers as of Chrome and Firefox.
+//       video.src = window.URL.createObjectURL(localMediaStream);
+//       Please refer to these:
+//       Depreceated  - https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+//       Newer Syntax - https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
+    
+    video.srcObject = localMediaStream;
+    video.play();
+  })
+  .catch(err => {
+    console.error(`OH NO!!!`, err);
   });
 }
+
+function paintToCanvas(){
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    }, 16);
+}
+
+function takePhoto(){
+  snap.currentTime = 0;
+  snap.play();
+
+  // Take the data out of the canvas.
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
