05f186ae9a9bb79023b8aa5f4035d1d9c195a92e
Trish Mederos <trishmederos@gmail.com>
2019-04-02 18:07:42 -0700

    added files

---
+++ 31 - Javascript/app.js
@@ -0,0 +1,23 @@
+
+loadEvents();
+// load every event in the page
+function loadEvents(){
+  document.querySelector('form').addEventListener('submit',submit);
+}
+// subit data function
+function submit(e){
+  e.preventDefault();
+  let input = document.querySelector('input');
+  if(input.value != '')
+    addTask(input.value);
+  input.value = '';
+}
+
+// add tasks
+function addTask(task){
+  let ul = document.querySelector('ul');
+  let li = document.createElement('li');
+  li.innerHTML = `<span class="delete">×</span><input type="checkbox"><label>${task}</label>`;
+  ul.appendChild(li);
+  document.querySelector('.tasksBoard').style.display = 'block';
+}
