d16238184ea491ce6a330fa0554cf26dd7c4a181
Trish Mederos <trishmederos@gmail.com>
2019-04-02 18:21:35 -0700

    updated files

--- 31 - Javascript/app.js
+++ 31 - Javascript/app.js
@@ -3,6 +3,8 @@ loadEvents();
 // load every event in the page
 function loadEvents(){
   document.querySelector('form').addEventListener('submit',submit);
+  document.getElementById('clear').addEventListener('click',clearList);
+  document.querySelector('ul').addEventListener('click',deleteOrTick);
 }
 // subit data function
 function submit(e){
@@ -13,6 +15,11 @@ function submit(e){
   input.value = '';
 }
 
+function clearList(e){
+  // setting the ul innerHML to an empty string
+  let ul = document.querySelector('ul').innerHTML = '';
+}
+
 // add tasks
 function addTask(task){
   let ul = document.querySelector('ul');
@@ -20,4 +27,32 @@ function addTask(task){
   li.innerHTML = `<span class="delete">×</span><input type="checkbox"><label>${task}</label>`;
   ul.appendChild(li);
   document.querySelector('.tasksBoard').style.display = 'block';
+}
+
+// deleteTick
+function deleteOrTick(e){
+  if(e.target.className == 'delete')
+    deleteTask(e);
+  else {
+    tickTask(e);
+  }
+}
+
+// delete task
+function deleteTask(e){
+  let remove = e.target.parentNode;
+  let parentNode = remove.parentNode;
+  parentNode.removeChild(remove);
+}
+
+// tick a task
+function tickTask(e){
+  const task = e.target.nextSibling;
+  if(e.target.checked){
+    task.style.textDecoration = "line-through";
+    task.style.color = "#ff0000";
+  }else {
+    task.style.textDecoration = "none";
+    task.style.color = "#2f4f4f";
+  }
 }
